ad8cceb8be16b03cd440ec7b7f8f0a746e9bddc3
RindratianaRatsimandavana <rindratianaratsimandavana@gmail.com>
2024-04-07 20:46:21 +0300

    Initialisation repository en remote

---
+++ src/main/java/mg/itu/ratsimandavanarindratiana/tp2ratsimandavanarindratiana/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package mg.itu.ratsimandavanarindratiana.tp2ratsimandavanarindratiana;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
